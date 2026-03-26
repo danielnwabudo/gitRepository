bc513a42ad1cd5054ef4dd3b7c26e56b030ff434
Daniel Nwabudo <danielnwabudo10@gmail.com>
2026-03-26 22:58:15 +0100

    Fixed correct Main.java file

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-Learning Git step by step
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Git practice!");
+        System.out.println("Learning Git step by step");
+    }
+}
+
